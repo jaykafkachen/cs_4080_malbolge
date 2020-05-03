9dbfbc7fcd7f53b26cb8de7bcc6eeb33f61c3671
Jay C <siennachen@cpp.edu>
2020-05-03 01:53:56 +0000

    Python code tested, user menu added

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -17,80 +17,106 @@ class Wallet:
 		self.balance = balance
 
 	def userMenu(self):
-		return None
+		while(True):
+			i = input("\nEnter choice below:\n1 - Deposit\n2 - Withdraw\n3 - Convert Currency\n4 - Print Wallet\n5 - Add Exchange Rates\n* - anything else to quit\n-->")
+			if i == "1":
+				amt =  float(input("How much to deposit (in decimal values)?\n-->"))
+				curr = input("What currency type (ex: USD, GBP)?\n-->")
+				self.deposit(curr, amt)
+			elif i == "2":
+				amt =  float(input("How much to withdraw (in decimal values)?\n-->"))
+				curr = input("What currency type (ex: USD, GBP)?\n-->")
+				self.withdraw(curr, amt)
+			elif i == "3":
+				amt =  float(input("Starting Currency\nDecimal value:\n-->"))
+				curr1 = input("Currency type:\n-->")
+				curr2 =  input("Target Currency Type:\n-->")
+				print(str(amt) + curr1 +" converted to "+str(self.convert(amt, curr1, curr2))+curr2)
+
+				currObj = Currency(amt, curr1)
+				print(str(currObj.amount)+str(currObj.currencyType) +" converted to " +str(self.convertCurrency(currObj, curr2).amount)+str(currObj.currencyType))
+
+			elif i == "4":
+				self.printWallet()
+			elif i == "5":
+				curr = input("Enter currency type to add:\n-->")
+				if curr in self.exchangeRate:
+					print(curr+" already added")
+				else:
+					self.exRateNotFound(curr)
+			else:
+				break
 
 	def deposit(self, currency, amt):
-		bal = self.balance[currency]
-		if bal is None:
+		if currency not in self.exchangeRate:
 			self.exRateNotFound(currency)
-			bal = 0
-		
-		self.balance[currency] = bal + amt
+			currentbal = 0
+		else:
+			currentbal = self.balance[currency]
+		self.balance[currency] = float(currentbal) + float(amt)
+		print("Deposited: "+str(amt)+"\nNew "+currency+" Balance: "+str(self.balance[currency]))
 	
 	def withdraw(self, currency, amt):
-		bal = self.balance[currency]
-		if bal is None:
+		if currency not in self.balance:
 			print("No balance available for: "+currency)
-		elif bal-amt < 0:
-			print("Current balance is "+bal+", cannot withdraw greater than this amount")
 		else:
-			bal -= amt
-			self.balance[currency] -= bal
-			print("Withdrew: "+amt+"\nNew "+currency+"Balance: "+bal)
+			bal = self.balance[currency]
+			if bal < amt:
+				print("Current "+currency+" balance is "+str(bal)+", cannot withdraw greater than this amount")
+			else:
+				bal = float(bal) - float(amt)
+				self.balance[currency] -= float(bal)
+				print("Withdrew: "+str(amt)+"\nNew "+currency+" Balance: "+str(bal))
 
 	def convert(self, amt, currFrom, currTo):
+		if currFrom not in self.exchangeRate:
+			self.exRateNotFound(currFrom)
+		if currTo not in self.exchangeRate:
+			self.exRateNotFound(currTo)
+
 		exrFrom = self.exchangeRate[currFrom]
 		exrTo = self.exchangeRate[currTo]
-		if exrFrom is None:
-			added = self.exRateNotFound(currFrom)
-		if exrTo is None:
-			added = added & self.exRateNotFound(currTo)
-		if added is False:
-			print("Could not convert, no suitable exchange rates found")
-			return -1
+		
 		converted = amt*exrTo/exrFrom
-		print(amt + " "+currFrom + " is " + converted +" "+currTo)
 		return converted
 
 	def convertCurrency(self, currencyFrom, currTo):
+		if currencyFrom.currencyType not in self.exchangeRate:
+			self.exRateNotFound(currencyFrom.currencyType)
+		if currTo not in self.exchangeRate:
+			self.exRateNotFound(currTo)
+
 		exrFrom = self.exchangeRate[currencyFrom.currencyType]
 		exrTo = self.exchangeRate[currTo]
-		if exrFrom is None:
-			added = self.exRateNotFound(currencyFrom.currencyType)
-		if exrTo is None:
-			added = added & self.exRateNotFound(currTo)
-		if added is False:
-			print("Could not convert, no suitable exchange rates found")
-			return -1
+
 		converted = currencyFrom.amount*exrTo/exrFrom
 		currencyFrom.amount = converted
 		currencyFrom.currencyType = currTo
-		print(currencyFrom + " was converted to " + converted +" "+currTo)
 		return currencyFrom
 
 	def addExchangeRate(self, currency, exRate):
 		self.exchangeRate[currency] = exRate
 
 	def exRateNotFound(self, currency):
-		ans = input("There is no exchange rate for: "+currency+ ", would you like to add it? (y or n)\n-->");
-		if ans == "Y" or "y":
-			exRate = input("What is the exchange rate from USD to "+currency+"?\n-->")
-			self.addExchangeRate(currency,exRate)
-			return True
-		else:
-			return False
+		exRate = float(input("What is the exchange rate from USD to "+currency+"?\n-->"))
+		self.addExchangeRate(currency,exRate)
+		return True
+			
 
 	def printWallet(self):
 		print(self.balance)
 
-	#testing values below
+#testing values below
 
-	#for pass by value 
-	bal = { "USD":20.0, "JPY":70023, "CNY":88888, "GBP":99.02 }
-	
-	#for pass by obj ref 
-	c1 = Currency(20.0, "USD")
-	c2 = Currency(70023, "JPY")
-	c3 = Currency(88888, "CNY")
-	c4 = Currency(99.02, "GBP")
-	bal = { c1:c1.amount, c2:c2.amount, c3:c3.amount, c4:c4.amount }
+#for pass by value 
+bal1 = { "USD":20.0, "JPY":70023, "CNY":88888, "GBP":99.02 }
+
+#for pass by obj ref 
+c1 = Currency(20.0, "USD")
+c2 = Currency(70023, "JPY")
+c3 = Currency(88888, "CNY")
+c4 = Currency(99.02, "GBP")
+bal2 = { c1:c1.amount, c2:c2.amount, c3:c3.amount, c4:c4.amount }
+
+w = Wallet(bal1)
+w.userMenu()
